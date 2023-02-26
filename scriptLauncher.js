93f07d5026862b4aaa4b71d3c582f4682b342dbd
Chester  Mealer <cdm014@gmail.com>
2023-02-26 01:31:45 -0500

    updated launcher

--- scriptLauncher.js
+++ scriptLauncher.js
@@ -1,12 +1,9 @@
 export async function main (ns) {
 	var scriptName =  ns.args[0];
-	var newArgs = new Array();
-	for (var index = 1; index < ns.args.length; index++) {
-		newArgs.push(ns.args[index]);
-	}
+	var newArgs = ns.args.slice(2);
 	var server = await ns.getHostname();
 	var maxRam = await ns.getServerMaxRam(server);
 	var reqRam = await ns.getScriptRam(scriptName);
 	var threads = Math.max(1,Math.floor(maxRam/reqRam));
-	await ns.spawn(scriptName,threads, newArgs);
+	await ns.spawn(scriptName,threads, ...newArgs);
 }
