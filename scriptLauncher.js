cd81d10df3a3da0b1aad03644412905b24e03b64
Chester  Mealer <cdm014@gmail.com>
2023-02-26 01:39:10 -0500

    fixed typo

--- scriptLauncher.js
+++ scriptLauncher.js
@@ -14,7 +14,7 @@ export async function main (ns) {
 	var scriptName =  ns.args[0];
 	var newArgs = ns.args.slice(2);
 	var server = await ns.getHostname();
-	await ns.kilall();
+	await ns.killall();
 	var maxRam = await ns.getServerMaxRam(server);
 	var reqRam = await ns.getScriptRam(scriptName);
 	var threads = Math.max(1,Math.floor(maxRam/reqRam));
