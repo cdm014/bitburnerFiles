eb8ce2952883564758f74be5fab877cbd59fcbed
Chester  Mealer <cdm014@gmail.com>
2023-02-26 01:37:19 -0500

    updated launcher

--- scriptLauncher.js
+++ scriptLauncher.js
@@ -1,7 +1,20 @@
+/*
+ * Name:
+ * scriptLauncher.js
+ *
+ * Parameters:
+ * script name - name of script to run on current host
+ * passable parameters - parameters for the script to be run
+ *
+ * Description: 
+ * runs the script whose name is the first argument as many times as ram allows
+ */
+
 export async function main (ns) {
 	var scriptName =  ns.args[0];
 	var newArgs = ns.args.slice(2);
 	var server = await ns.getHostname();
+	await ns.kilall();
 	var maxRam = await ns.getServerMaxRam(server);
 	var reqRam = await ns.getScriptRam(scriptName);
 	var threads = Math.max(1,Math.floor(maxRam/reqRam));
