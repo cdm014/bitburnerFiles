8233ab8b2c1cd33cfcec9076db14c1687e86ec71
Chester  Mealer <cdm014@gmail.com>
2023-02-26 01:42:11 -0500

    .

--- scriptLauncher.js
+++ scriptLauncher.js
@@ -14,7 +14,6 @@ export async function main (ns) {
 	var scriptName =  ns.args[0];
 	var newArgs = ns.args.slice(2);
 	var server = await ns.getHostname();
-	await ns.killall();
 	var maxRam = await ns.getServerMaxRam(server);
 	var reqRam = await ns.getScriptRam(scriptName);
 	var threads = Math.max(1,Math.floor(maxRam/reqRam));
