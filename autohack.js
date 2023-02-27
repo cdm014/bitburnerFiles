172d8b5b52bcd5f0802cd781a0f57c8e856eadf5
Chester  Mealer <cdm014@gmail.com>
2023-02-26 19:35:44 -0500

    updated autohack script

--- autohack.js
+++ autohack.js
@@ -12,56 +12,62 @@
 
 export async function main(ns) {
 	var server = ns.args[0];
-	//Nuke our target or try
-  var NukeTries = 5;
-	while (NukeTries > 0) {
-		var hasRootAccess = await ns.hasRootAccess(server);
-		if (!hasRootAccess) {
-			await nuke(server);
-		}
-		NukeTries--;
-	}
+	
 	var hasRootAccess = await ns.hasRootAccess(server);
 
 	if (hasRootAccess) {
 		//if we have root access configure some info so we can know what to do
 		var MaxMoney = await ns.getServerMaxMoney(server);
-		var MoneyThresh = MaxMoney *.75;
+		var MoneyThresh = MaxMoney *.75; //we want a minimum of 75% of the maximum money available
+		
 		var MinSec = await ns.getServerMinSecurityLevel(server);
-		var SecGoal = MinSec * 1.5;
+		//start weakening if we're at double the minimum security level
+		var SecGoal = MinSec ;
 		var SecThresh = MinSec * 2;
 		var vserver = await ns.getHostname();
+		var weakeningFlag = false;
+		var growingFlag = false;
 		while(true) {
 			//main loop
-			//execute an action 5 times so we're not constantly re-evaluating
-			await ns.tprint(vserver + " Sec: "+ns.getServerSecurityLevel(server)+"/"+MinSec);
-			await ns.tprint(vserver + " Money: "+ns.getServerMoneyAvailable(server)+"/"+MaxMoney);
-			//if money is too low grow it
-			if (ns.getServerMoneyAvailable(server) < MoneyThresh) {
-				while (ns.getServerMoneyAvailable(server) < MaxMoney *.9)
-				{
-					await ns.tprint(vserver+" Growing");
-					await ns.tprint(vserver+" Money: "+ns.getServerMoneyAvailable(server)+"/"+MaxMoney);
-					await ns.grow(server);
-				}
-			} else if (ns.getServerSecurityLevel(server) > SecThresh)
-			{
-				while (ns.getServerSecurityLevel(server) > SecGoal) 
-				{
-					await ns.tprint(vserver+" weaken");
-					await ns.tprint(vserver+" Sec: "+ns.getServerSecurityLevel(server)+"/"+MinSec);
-					await ns.weaken(server);
-				}
-			} else 
-			{
-				while (ns.getServerMoneyAvailable(server) > MoneyThresh && ns.getServerSecurityLevel(server) < SecThresh) 
-				{
-					await ns.tprint(vserver+" hacking "+server);
-					await ns.hack(server);
-				}
+			// goal priorities
+			// 1. weaken first so other actions take less time
+			// 2. grow money to maximize income
+			// 3. hack when other goals are met
+			var currentSecurity = await ns.getServerSecurityLevel(server);
+			var currentMoney = await ns.getServerMoneyAvailable(server);
+			if (currentSecurity > SecThresh) {
+				weakeningFlag = true;
+			} else if (currentSecurity == MinSec) {
+				weakeningFlag = false;
 			}
-
+			if (currentMoney < MoneyThresh) {
+				growingFlag = true;
+			} else if (currentMoney == MaxMoney) {
+				growingFlag = false;
+			}
+			if (weakeningFlag == true) {
+				await weaken(server, vserver, ns);
+			} else if (growingFlag == true) {
+				await grow(server, vserver, ns);
+			} else {
+				await hack(server, vserver, ns);
+			}
 		}
-
 	}
+}
+
+async function weaken(server, vserver, ns) {
+	await ns.tprint(vserver +" weakening " + server);
+	await ns.weaken(server);
+}
+			
+
+async function grow (server, vserver, ns) {
+	await ns.tprint(vserver + " growing " +server);
+	await ns.grow(server);
+}
+
+async function hack (server, vserver, ns) {
+	await ns.tprint(vserver + " hacking " + server);
+	await ns.hack(server);
 }
