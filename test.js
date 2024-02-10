37b7630d4183eb91bf5665a449434340d1542872
= <=>
2024-02-10 16:33:31 -0500

    .

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 
 import {getProgramLevel} from "library";
 export async function main(ns) {
- var x = hackAnalyzePercent("foodnstuff");
+ var x = await ns.hackAnalyze("foodnstuff");
  await ns.tprint("hackpercent");
 }
